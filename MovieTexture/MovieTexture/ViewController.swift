0ee2e461d181afff8dbd3da760e63a577e5ce24e
Dmitriy Safarov <kazdevelop@gmail.com>
2019-07-28 18:16:38 +0600

    Initial commit

---
+++ MovieTexture/MovieTexture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieTexture
+//
+//  Created by Dmitriy Safarov on 28/07/2019.
+//  Copyright © 2019 SimpleCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
